ff0060a9913110a05008142ef1314ec1f6ba95dd
vivek gupta <vivekgpt107@gmail.com>
2024-10-20 17:01:23 +0530

    added footer links

--- src/components/page/footer/Footer.tsx
+++ src/components/page/footer/Footer.tsx
@@ -3,14 +3,29 @@ import { Linkedin } from "lucide-react";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
 import { ExternalLink } from "lucide-react";
+import Link from "next/link";
+import { github, linkedin, portfolio } from "@/constants/footerLinks";
 
 const Footer = () => {
     return ( <div className="flex flex-col gap-2 items-center justify-center h-full text-muted-foreground">
         <p>Inspired by start.spring.io.</p>
         <div className="flex gap-2">
-            <Button variant={"ghost"} className="p-0 h-5" aria-label="portfolio page"> <ExternalLink className="size-5" /> </Button>
-            <Button variant={"ghost"} className="p-0 h-5" aria-label="LinkedIn profile"> <FaLinkedin className="size-5 text-[#0a66c2] hover:text-[#4284c7] transition-all" /> </Button>
-            <Button variant={"ghost"} className="p-0 h-5" aria-label="Github profile"> <FaGithub className="size-5" /> </Button>
+            <Link href={portfolio}>
+                <Button variant={"ghost"} className="p-0 h-5" aria-label="portfolio page"> <ExternalLink className="size-5" /> 
+                <div className="sr-only">About me</div>
+                </Button>
+            </Link>
+            <Link href={linkedin}>
+                <Button variant={"ghost"} className="p-0 h-5" aria-label="portfolio page"> <FaLinkedin className="size-5 text-[#4284c7] hover:text-[#0a66c2] transition-all" /> 
+                <div className="sr-only">LinkedIn profile</div>
+                </Button>
+            </Link>
+            <Link href={github}>
+                <Button variant={"ghost"} className="p-0 h-5" aria-label="portfolio page"> <FaGithub className="size-5" /> 
+                <div className="sr-only">Github profile</div>
+                </Button>
+            </Link>
+            
         </div>
     </div> );
 }
