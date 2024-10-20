5de56bec7b241c943fb84fc929a0f13d80a06e09
vivek gupta <vivekgpt107@gmail.com>
2024-10-20 17:51:10 +0530

    added pagination improvements

--- src/components/page/infoDialog/InfoButton.tsx
+++ src/components/page/infoDialog/InfoButton.tsx
@@ -1,35 +1,52 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 
 const InfoButton = () => {
-    return ( 
-        <Popover>
-            <PopoverTrigger asChild>
-    <Button className="p-0 h-5 text-muted-foreground" variant={"ghost"}>
-        <QuestionMarkCircledIcon className="size-5" />
-    </Button>
-    </PopoverTrigger>
-    <PopoverContent className="w-80 bg-[hsl(var(--background))]">
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="p-0 h-5 text-muted-foreground" variant={"ghost"}>
+          <QuestionMarkCircledIcon className="size-5" />
+          <p className="sr-only">Help on quick filters</p>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-80 bg-[hsl(var(--background))]">
         <div className="flex flex-col gap-2">
-            <div className="text-lg font-semibold text-primary">Quick Filters</div>
-            <div className="text-sm text-muted-foreground">Quick filters can be added by typing in the search bar.</div>
-            <div className="text-sm text-muted-foreground font-semibold">
-                Examples:
-            </div>
-            <div className="text-sm text-muted-foreground">
-                <ul>
-                    <li>1. <span className="text-primary">react</span> : adds filter for react package</li>
-                    <li>2. <span className="text-primary">react@18</span> : adds filter for react package with version 18</li>
-                    <li>
-                        3. <span className="text-primary">react next tailwindcss</span> : adds filter for react, next and tailwindcss packages
-                    </li>
-                </ul>
-            </div>
+          <div className="text-lg font-semibold text-primary">
+            Quick Filters
+          </div>
+          <div className="text-sm text-muted-foreground">
+            Quick filters can be added by typing in the search bar.
+          </div>
+          <div className="text-sm text-muted-foreground font-semibold">
+            Examples:
+          </div>
+          <div className="text-sm text-muted-foreground">
+            <ul>
+              <li>
+                1. <span className="text-primary">react</span> : adds filter for
+                react package
+              </li>
+              <li>
+                2. <span className="text-primary">react@18</span> : adds filter
+                for react package with version 18
+              </li>
+              <li>
+                3. <span className="text-primary">react next tailwindcss</span>{" "}
+                : adds filter for react, next and tailwindcss packages
+              </li>
+            </ul>
+          </div>
         </div>
-    </PopoverContent>
-    </Popover> );
-}
- 
+      </PopoverContent>
+    </Popover>
+  );
+};
+
 export default InfoButton;
--- src/components/page/viewFilteredTemplate/pagination.tsx
+++ src/components/page/viewFilteredTemplate/pagination.tsx
@@ -7,7 +7,7 @@ const MyPagination: React.FC<{take: number; skip: number; count: number; searchP
     const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
     return  pages.length > 1 && <div className="flex gap-2">
         {pages.map((page) => (
-            <Link key={page} href={{ query: { ...searchParams, skip: (page - 1) * take } }}>
+            <Link key={page} href={{ query: { ...searchParams, skip: (page - 1) * take } }} scroll={false}>
                 <div className={cn("px-2 py-0.5 rounded-md", { "border-primary border": currentPage === page })}>
                     {page}
                 </div>
