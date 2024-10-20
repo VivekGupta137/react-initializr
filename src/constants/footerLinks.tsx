6a1419433b101dfe382ffc611e7e54fc931c3f9f
vivek gupta <vivekgpt107@gmail.com>
2024-10-20 17:11:34 +0530

    updated the portfolio page

--- src/constants/footerLinks.tsx
+++ src/constants/footerLinks.tsx
@@ -1,3 +1,3 @@
-export const portfolio = "vivekblog-omega.vercel.app"
+export const portfolio = "https://vivekblog-omega.vercel.app/"
 export const linkedin = "https://www.linkedin.com/in/vivek-gupta-visa/"
 export const github = "https://github.com/VivekGupta137"
