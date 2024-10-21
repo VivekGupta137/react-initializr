36d01432c621a28e67f9218ecd6f203048020753
vivek gupta <vivekgpt107@gmail.com>
2024-10-21 19:43:34 +0530

    added github repo link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Header from "@/components/page/header/Header";
 import ViewFilteredTemplate from "@/components/page/viewFilteredTemplate";
 import { Suspense } from "react";
 import RadioFilters from "@/components/page/RadioFilters";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import Footer from "@/components/page/footer/Footer";
 import AddTemplate from "@/components/page/addTemplate/AddTemplate";
 import ViewFilterLoader from "@/components/page/viewFilteredTemplate/ViewFilterLoader";
-import SortTemplates from "@/components/page/viewFilteredTemplate/sortTemplates";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface HomeProps {
   searchParams: Record<string, string>;
@@ -15,10 +15,11 @@ interface HomeProps {
 
 export default async function Home({ searchParams }: HomeProps) {
   return (
-    <div className="container m-auto sm:px-5 md:px-20 min-h-lvh">
+    <div className="container m-auto min-h-lvh relative">
+      <div className="sm:px-5 md:px-20 ">
       <div className="border-l border-r px-5 min-h-lvh flex flex-col h-full">
         <Header />
-
+        
         <div className="flex gap-0 sm:px-0 flex-col lg:flex-row pb-20 self-stretch grow">
           <div className="basis-1/2 shrink-0 my-10 lg:border-r-1">
             <RadioFilters searchParams={searchParams} />
@@ -43,13 +44,19 @@ export default async function Home({ searchParams }: HomeProps) {
         </div>
 
         <div className="fixed bottom-0 left-0 h-20 w-full">
-          <div className="container m-auto sm:px-5 md:px-20 h-full ">
+          <div className="container m-auto sm:px-5 md:px-20 h-full relative ">
+          <div className="md:absolute pr-5 -translate-x-full bottom-5">
+            <Link href="https://github.com/VivekGupta137/react-initializr" aria-label="github url" target="_blank">
+              <GitHubLogoIcon className="size-10"/>
+            </Link>
+          </div>
             <div className="w-full bg-[#23272F] h-full border-l border-r ">
               <Footer />
             </div>
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
