74c12bf9a4ec9526e05f0693603f48e0781005f6
vivek gupta <vivekgpt107@gmail.com>
2024-10-25 18:29:11 +0530

    enhacement to star button

--- src/components/page/claps/ClapCounter.tsx
+++ src/components/page/claps/ClapCounter.tsx
@@ -2,13 +2,14 @@
 import { FaHandsClapping } from "react-icons/fa6";
 import { Button } from "@/components/ui/button";
 import { addClap, getClaps } from "@/actions/action";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { Heart, StarIcon } from "lucide-react";
 import { HeartFilledIcon, StarFilledIcon } from "@radix-ui/react-icons";
 import {motion} from "framer-motion";
 import SuperStar from "./SuperStar";
 const ClapCounter = () => {
   const [claps, setClaps] = useState(0);
+  const [isPending, startTransition] = useTransition();
   const userClapCount =
     typeof window !== "undefined"
       ? parseInt(localStorage.getItem("clapCount") as string) || 0
@@ -20,6 +21,12 @@ const ClapCounter = () => {
     });
   }, []);
 
+  const handleOnClap = async () => {
+    const clapsCount = await addClap();
+    localStorage.setItem("clapCount", userClapCount + 1 + "");
+    setClaps(clapsCount.clap);
+  }
+
   return (
     <motion.div layout className="flex justify-center md:flex-col items-center gap-1">
       <Button
@@ -27,11 +34,8 @@ const ClapCounter = () => {
         variant={"ghost"}
         disabled={userClapCount >= 10}
         className="rounded-md relative size-8"
-        onClick={async () => {
-          const clapsCount = await addClap();
-          setClaps(clapsCount.clap);
-          localStorage.setItem("clapCount", userClapCount + 1 + "");
-        }}
+        loading={isPending}
+        onClick={()=>startTransition(()=>handleOnClap())}
       >
         <SuperStar />
       </Button>
